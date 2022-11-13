214b50568d5f33f0677530a60eff3ee199365ee0
Tim-ats-d <tim.arnouts@protonmail.com>
2022-11-13 21:55:27 +0100

    Implement `respond_document`.

--- lib/request.ml
+++ lib/request.ml
@@ -1,6 +1,8 @@
-type t = { addr : Lwt_unix.sockaddr; uri : Uri.t }
+module StringMap = Map.Make (String)
 
-let make ~uri ~addr = { uri; addr }
+type t = { addr : Lwt_unix.sockaddr; uri : Uri.t; params : string StringMap.t }
+
+let make ~uri ~addr = { uri; addr; params = StringMap.empty }
 let uri { uri; _ } = uri
 
 let addr { addr; _ } =
