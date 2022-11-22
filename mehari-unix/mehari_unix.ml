e9e1bfe62addc051918bfaa57244ba7b7c7d4807
ArtichautCosmique <lelolartichaut@laposte.net>
2022-11-22 22:14:54 +0100

    Add address parameters.

--- mehari-unix/mehari_unix.ml
+++ mehari-unix/mehari_unix.ml
@@ -1,12 +1,21 @@
 open Lwt.Syntax
 include Mehari.Make (Pclock) (Mirage_kv_unix) (Tcpip_stack_socket.V4V6)
 
-let run_lwt ?port ?certchains callback =
+let stack ~v4 ~v6 =
+  Tcpip_stack_socket.V4V6.TCP.connect
+    ~ipv4_only:(match v6 with None -> true | _ -> false)
+    ~ipv6_only:false v4 v6
+
+let run_lwt ?port ?certchains ?v4 ?v6 callback =
   let* stack =
-    Tcpip_stack_socket.V4V6.TCP.connect ~ipv4_only:false ~ipv6_only:false
-      Ipaddr.V4.Prefix.loopback None
+    stack
+      ~v4:
+        (match v4 with
+        | Some ip -> Ipaddr.V4.Prefix.of_string_exn ip
+        | None -> Ipaddr.V4.Prefix.loopback)
+      ~v6:(Option.map Ipaddr.V6.Prefix.of_string_exn v6)
   in
   run_lwt ?port ?certchains stack callback
 
-let run ?port ?certchains callback =
-  run_lwt ?port ?certchains callback |> Lwt_main.run
+let run ?port ?certchains ?v4 ?v6 callback =
+  run_lwt ?port ?certchains ?v4 ?v6 callback |> Lwt_main.run
