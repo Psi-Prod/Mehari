a788a8f7db4c114fc3719114d1373907fd76a0f2
Tim-ats-d <tim.arnouts@protonmail.com>
2023-01-06 22:43:48 +0100

    Little fix in rate limiter.

--- mehari/rate_limiter_impl.ml
+++ mehari/rate_limiter_impl.ml
@@ -22,20 +22,16 @@ module Make (Clock : Mirage_clock.PCLOCK) (IO : Types.IO) (Addr : Types.ADDR) :
     mutable history : int AddrMap.t;
   }
 
+  let now () =
+    let _, ps = Clock.now_d_ps () in
+    Int64.div ps (Int64.of_float (10. ** 12.)) |> Int64.to_int
+
   let reset t =
-    let now =
-      let _, ps = Clock.now_d_ps () in
-      Int64.to_int ps / 1_000_000_000_000
-    in
-    t.next_timestamp <- now + t.period;
+    t.next_timestamp <- now () + t.period;
     t.history <- AddrMap.empty
 
   let check t req =
-    let now =
-      let _, ps = Clock.now_d_ps () in
-      Int64.div ps (Int64.of_float (10. ** 12.))
-    in
-    let time_left = t.next_timestamp - Int64.to_int now in
+    let time_left = t.next_timestamp - now () in
     if time_left <= 0 then reset t;
     let addr = Request.ip req in
     let n = AddrMap.find_opt addr t.history |> Option.fold ~none:1 ~some:succ in
