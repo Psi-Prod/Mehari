449356a1e27b05ee4f4a0c1dc1dfc15491c25af9
Tim ats <tim.arnouts@protonmail.com>
2023-02-06 00:58:16 +0100

    Add a MirageOS unikernel example (#61)

--- examples/README.md
+++ examples/README.md
@@ -11,3 +11,16 @@
 - [stream](stream.ml) — take advantage of Eio's direct style to setup a request handler to stream a response body instead of plain text.
 - [cgi](cgi.ml) — demonstrates CGI support. Don't forget to make `cgi_script.py` executable.
 - [vhost](vhost.ml) — demonstrates the virtual hosting support.
+
+## Unikernel
+
+[unikernel/](unikernel/) is a simple unikernel which serves files located in [unikernel/gemtext/](unikernel/gemtext/).
+Copy your private key in [unikernel/tls/server.key](unikernel/tls/server.key) and your certificate in [unikernel/tls/server.pem](unikernel/tls/server.pem) and run the following:
+
+```bash
+$ cd unikernel/
+$ mirage configure -t unix # initial setup for UNIX backend
+$ make depend # install dependencies
+$ make # build the program
+$ ./gemini-srv # run the program
+```
---
+++ examples/unikernel/config.ml
@@ -0,0 +1,34 @@
+open Mirage
+
+let port =
+  let doc =
+    Key.Arg.info
+      ~doc:"The TCP port on which to listen for incoming connections."
+      [ "port" ]
+  in
+  Key.(create "port" Arg.(opt int 1965 doc))
+
+let static_key = Key.(value @@ kv_ro ~group:"static" ())
+let static = generic_kv_ro ~key:static_key "gemtext"
+let certs_key = Key.(value @@ kv_ro ~group:"certs" ())
+let certs = generic_kv_ro ~key:certs_key "tls"
+
+let main =
+  foreign
+    ~keys:Key.[ v port ]
+    ~packages:
+      [
+        package "mehari" ~pin:"https://github.com/Psi-Prod/Mehari.git#dev";
+        package "mehari-mirage"
+          ~pin:"https://github.com/Psi-Prod/Mehari.git#dev";
+      ]
+    "Unikernel.GeminiServer"
+    (random @-> kv_ro @-> kv_ro @-> pclock @-> stackv4v6 @-> time @-> job)
+
+let () =
+  register "gemini-srv"
+    [
+      main $ default_random $ static $ certs $ default_posix_clock
+      $ generic_stackv4v6 default_network
+      $ default_time;
+    ]
