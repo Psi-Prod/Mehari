f3f3046542e23da4f2b33618a378c04153c08224
removedporn <86824510+removedporn@users.noreply.github.com>
2023-05-09 14:14:56 +0800

    Delete examples directory

--- examples/cgi_script.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-
-print("20 text/gemtext\r\n")
-print("Hello from a CGI script!")
--- examples/hello.ml
+++
@@ -1,9 +0,0 @@
-open Lwt.Infix
-
-let main () =
-  X509_lwt.private_of_pems ~cert:"cert.pem" ~priv_key:"key.pem"
-  >>= fun certchain ->
-  (fun _ -> Mehari_lwt_unix.respond_text "Hello")
-  |> Mehari_lwt_unix.run_lwt ~certchains:[ certchain ]
-
-let () = Lwt_main.run (main ())
